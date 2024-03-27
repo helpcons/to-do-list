d2b5a6d0b0a69a4b9a7a22e5b9cc57c04720322e
Helder Pereira <helder_leg@hotmail.com>
2024-03-27 17:16:47 -0300

    https://github.com/helpcons/to-do-list

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Crear um botão de fechar 
+// Crie um botão "fechar" e anexe-o a cada item da lista
 var myNodelist = document.getElementsByTagName("li");
 var i;
 
@@ -10,4 +10,53 @@ for (i = 0; i < myNodelist.length; i++) {
     myNodelist[i].appendChild(span);
 }
 
-console.log(myNodelist);
+
+// Clique em um botão Fechar para ocultar o item atual da lista
+var close = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+// Adicione um símbolo de "marcado" ao clicar em um item da lista
+var list = document.querySelector('ul');
+
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+
+// Cria um novo item da lista ao clicar no botão "Adicionar"
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Você deve escrever alguma coisa!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    // Limpa o texto do input?
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
